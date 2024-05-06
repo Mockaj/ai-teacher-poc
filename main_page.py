794410fc26b526fe08164e06f487c1b24c45c41e
Ladislav Husty <lada.husty@gmail.com>
2024-05-06 11:41:33 +0200

    removed prompt window from the main page

--- main_page.py
+++ main_page.py
@@ -19,22 +19,3 @@ show_pages(
         Page("other_pages/buro_conv.py", "• Gespräch"),
     ]
 )
-
-input = st.text_area('Prompt...', key='input')
-submit_button = st.button('Submit', type='primary')
-box = st.empty()
-
-if submit_button and input:
-    st.session_state.chat_history.append(('user', input))
-    response = openai_st.stream(
-        model='gpt-4-turbo-preview', prompt=input, box=box)
-    st.session_state.chat_history.append(('ai', response))
-
-with st.expander("Chat History"):
-    for speaker, text in reversed(st.session_state.chat_history):
-        if speaker == 'user':
-            col1, col2 = st.columns([0.8, 0.2])
-            col1.markdown(f"**You:** {text}")
-        elif speaker == 'ai':
-            col1, col2 = st.columns([0.2, 0.8])
-            col2.markdown(f"**AI:** {text}", unsafe_allow_html=True)
